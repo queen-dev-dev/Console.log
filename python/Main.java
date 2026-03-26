4de59194fd4a8cdf1ab06cfd29f115bf2e87631d
Devon <100230169@student.burnley.ac.uk>
2026-03-26 11:15:43 +0000

    inferior to js but whatever

---
+++ python/Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public void log (String message) {
+        System.out.println(message);
+    }
+    
+    public static void main(String[] args) {
+        Main console = new Main();
+        console.log("sigma");
+    }
+}
